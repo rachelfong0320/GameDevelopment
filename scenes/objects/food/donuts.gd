3815788d2c289fac453d1d992a85ccce4fc2fd0d
rachelfong0320 <rachelfong.tw@gmail.com>
2025-05-20 01:16:20 +0800

    Shaking effect of item

--- scenes/objects/food/donuts.gd
+++ scenes/objects/food/donuts.gd
@@ -6,6 +6,7 @@ extends Sprite2D
 func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
 	damage_component.max_damaged_reached.connect(on_max_damaged_reached)
+	material.set_shader_parameter("shake_intensity",0.5)
 	
 func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
