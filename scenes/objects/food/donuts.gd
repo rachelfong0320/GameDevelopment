134cbac696cc8590838b292f79380ba2c67615a4
rachelfong0320 <rachelfong.tw@gmail.com>
2025-05-19 22:21:50 +0800

    Added donuts

---
+++ scenes/objects/food/donuts.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+@onready var hurt_component: HurtComponent= $HurtComponent
+@onready var damage_component: DamageComponent= $DamageComponent
+
+func _ready() -> void:
+	hurt_component.hurt.connect(on_hurt)
+	damage_component.max_damaged_reached.connect(on_max_damaged_reached)
+	
+func on_hurt(hit_damage: int) -> void:
+	damage_component.apply_damage(hit_damage)
+	
+func on_max_damaged_reached()-> void:
+	print("max damaged reached")
+	queue_free()
