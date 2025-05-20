e57d1b404432c67654f7ce83517fb67d105e1d11
rachelfong0320 <rachelfong.tw@gmail.com>
2025-05-20 16:37:43 +0800

    Place food in map
    Rotten food and fresh food

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -4,9 +4,26 @@ extends Area2D
 @export var tool: DataTypes.Tools = DataTypes.Tools.None
 
 signal hurt
+signal fsktm_item_collected
+signal food_collected
 
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component = area as HitComponent
 	
-	if tool == hit_component.current_tool:
-		hurt.emit(hit_component.hit_damage)
+	if not hit_component:
+		return
+	
+	match tool:
+		DataTypes.Tools.Food:
+			print("Food collected")
+			food_collected.emit()
+			get_parent().queue_free()
+		
+		DataTypes.Tools.fsktmItem:
+			print("Fsktm item collected - damaging and emitting signal")
+			hurt.emit(hit_component.hit_damage)
+			fsktm_item_collected.emit()
+			get_parent().queue_free()
+		
+		_:
+			print("Unhandled tool type:", tool)
---
+++ trash.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+@onready var hurt_component: HurtComponent= $HurtComponent
+@onready var damage_component: DamageComponent= $DamageComponent
+
+func _ready() -> void:
+	hurt_component.hurt.connect(on_hurt)
+	hurt_component.fsktm_item_collected.connect(on_fsktm_item_collected)
+	damage_component.max_damaged_reached.connect(on_max_damaged_reached)
+	
+func on_hurt(hit_damage: int) -> void:
+	damage_component.apply_damage(hit_damage)
+	
+func on_max_damaged_reached()-> void:
+	print("max damaged reached")
+	queue_free()
+
+
+func on_fsktm_item_collected():
+	print("Time deduction triggered")
+	# Call time manager or emit a higher-level signal
