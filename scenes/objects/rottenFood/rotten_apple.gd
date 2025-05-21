783e438c904f166a206d1c20d8ba2f977690ecd0
rachelfong0320 <rachelfong.tw@gmail.com>
2025-05-21 15:41:09 +0800

    Added rotten apple

--- scenes/objects/rottenFood/rotten_apple.gd
+++ scenes/objects/rottenFood/rotten_apple.gd
@@ -1,10 +1,10 @@
 extends Sprite2D
-@onready var hurt_component = $HurtComponent
+
 @onready var damage_component = $DamageComponent
+@onready var hurt_component =  $HurtComponent
 
 func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
-	hurt_component.fresh_food_collected.connect(on_fresh_food_collected)
 	damage_component.max_damaged_reached.connect(on_max_damaged_reached)
 	material.set_shader_parameter("shake_intensity",0.5)
 	
@@ -14,8 +14,3 @@ func on_hurt(hit_damage: int) -> void:
 func on_max_damaged_reached()-> void:
 	print("max damaged reached")
 	queue_free()
-
-
-func on_fresh_food_collected():
-	print("Time deduction triggered")
-	# Call time manager or emit a higher-level signal
